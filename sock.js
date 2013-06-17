07ca3fe7f3e8f0bb57f0d22a11f785ef6c5a72da
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-17 15:29:33 +0100

    convert http to ws, to be more fool-proof

--- sock.js
+++ sock.js
@@ -3,5 +3,10 @@ var sock = require('sockjs-stream')
 
 module.exports = require('./inject')(function (){
   var args = [].slice.call(arguments)
+      .map(function (e) {
+        if('string' !== typeof e) return e
+        return e.replace(/^http/, 'ws')
+      })
+
   return sock.apply(null, args)
 })
